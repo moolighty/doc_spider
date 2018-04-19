926bf273c8f82021c22b68a6770c524dd24e43f1
moolighty <moolighty@yahoo.com>
2018-04-19 08:44:04 +0800

    提交爬取

--- doc_spider/settings.py
+++ doc_spider/settings.py
@@ -13,13 +13,13 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 2
+DOWNLOAD_DELAY = 3
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 16
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
@@ -28,10 +28,10 @@ DOWNLOAD_DELAY = 2
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
+DEFAULT_REQUEST_HEADERS = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+}
 
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
--- doc_spider/spiders/shanghai.py
+++ doc_spider/spiders/shanghai.py
@@ -11,14 +11,29 @@ from doc_spider.util.coding_conversion import gbk2utf8, gbk2unicode
 class ShanghaiSpider(scrapy.Spider):
     name = 'shanghai'
     allowed_domains = ['www.shanghai.gov.cn']
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'Accept-Encoding': 'gzip, deflate',
+        'Connection': 'keep-alive',
+        'Host': 'service.shanghai.gov.cn',
+        'Referer': 'http://www.shanghai.gov.cn/nw2/nw2314/nw2319/nw12344/index.html',
+        'Upgrade-Insecure-Requests': '1',
+        'Cookie': 'ASP.NET_SessionId=jebjcbwj1ewg5bkluq3a5j2s',
+    }
+    start_page = 1
+    end_page = 1198;
+    url = 'http://service.shanghai.gov.cn/pagemore/iframePagerIndex_12344_2_22.html?objtype=2&nodeid=12344&page={}&pagesize=22'
 
     def start_requests(self):
-        url = 'http://www.shanghai.gov.cn/nw2/nw2314/nw2319/nw12344/index.html'
-        yield Request(url)
+        for i in range(self.start_page, self.end_page + 1):
+            url = self.url.format(i)
+            yield Request(url, headers=self.headers)
 
     def parse(self, response):
         body = gbk2unicode(response.body)
-        sl = Selector(text=body).css('div#pageList ul.uli14.pageList li a')
+        sl = Selector(text=body).css('ul.uli14.pageList li a')
+        print(len(sl))
         for s in sl:
             href = s.xpath('@href').extract_first()
             url = response.urljoin(href)
@@ -28,7 +43,8 @@ class ShanghaiSpider(scrapy.Spider):
     
     def parse_announcement(self, response):
         body = gbk2unicode(response.body)
-        print(response.meta['title'])
+        article = Selector(text=body).xpath('//*[@id="ivs_content"]').extract()
+        print(article, response.url)
         
 if __name__ == '__main__':
     from scrapy.cmdline import execute
